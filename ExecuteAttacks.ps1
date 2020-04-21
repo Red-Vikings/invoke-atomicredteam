d7ab788cd6c1a3727de60e31ab32612244d6f34c
greenlion7 <heljijt@live.fr>
2020-04-21 20:29:30 +0200

    Added files to execute severaltechniques

---
+++ ExecuteAttacks.ps1
@@ -0,0 +1,11 @@
+﻿param([Parameter(Mandatory)][String[]] $techniques)
+
+Import-Module ".\Invoke-AtomicRedTeam.psd1" -Force
+$PSDefaultParameterValues = @{"Invoke-AtomicTest:PathToAtomicsFolder"="..\atomics"}
+
+$AtomicTests = $techniques
+Get-ChildItem $PathToAtomicsFolder -Recurse -Filter *.yaml -File | ForEach-Object {
+    $currentTechnique = [System.IO.Path]::GetFileNameWithoutExtension($_.FullName)
+    if ( $currentTechnique -ne "index" ) { $AtomicTests.Add($currentTechnique) | Out-Null }
+}
+$AtomicTests.GetEnumerator() | Foreach-Object { .\ExecuteSingleAttack.ps1 $_ }
---
+++ ExecuteSingleAttack.ps1
@@ -0,0 +1,5 @@
+﻿param([Parameter(Mandatory)][String] $technique)
+
+
+Invoke-AtomicTest -AtomicTechnique $technique -TestNumbers $test -GetPrereqs
+Invoke-AtomicTest -AtomicTechnique $technique -TestNumbers $test -ExecutionLogPath csvOutput.csv
