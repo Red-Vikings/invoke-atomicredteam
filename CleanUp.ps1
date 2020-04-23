11ada0189d2219f2488ea36d25d222edbb0c5fc3
greenlion7 <heljijt@live.fr>
2020-04-23 03:24:33 +0200

    Added a clean-up script

---
+++ CleanUp.ps1
@@ -0,0 +1,15 @@
+﻿param([Parameter(Mandatory)][String[]] $techniques)
+
+Import-Module ".\Invoke-AtomicRedTeam.psd1" -Force
+$PSDefaultParameterValues = @{"Invoke-AtomicTest:PathToAtomicsFolder"="..\atomics"}
+
+if ($techniques.ToUpper() -eq "ALL") {
+    Invoke-AtomicTest -AtomicTechnique "All" -Cleanup
+}
+
+$AtomicTests = $techniques
+Get-ChildItem $PathToAtomicsFolder -Recurse -Filter *.yaml -File | ForEach-Object {
+    $currentTechnique = [System.IO.Path]::GetFileNameWithoutExtension($_.FullName)
+    if ( $currentTechnique -ne "index" ) { $AtomicTests.Add($currentTechnique) | Out-Null }
+}
+$AtomicTests.GetEnumerator() | Foreach-Object { Invoke-AtomicTest $_ -Cleanup }
